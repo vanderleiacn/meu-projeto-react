deedc472bad8f5e71107c15eab77368a255a1d92
vanderleiacn <vanderleiacn@gmail.com>
2023-08-16 18:34:17 -0300

    utilizando renderizacao por condicao

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 
 import './App.css';
-//import HelloWorld from './components/HelloWorld';
+import Condicional from './components/Condicional';
 import Evento from './components/Evento';
 import Form from './components/Form';
-{/*import SayMyName from './components/SayMyName';*/}
+//import HelloWorld from './components/HelloWorld';
+//import SayMyName from './components/SayMyName';
 function App() {
   return (
 
     <div className="App"> {/*a tag div é usada para encapsular tudo*/}
+      <h1>Renderização Condicional</h1>
+      <Condicional/>
       <h1>Testando Eventos</h1> {/* declarado aqui, porem esta vindo do arquivo index.css*/}
       <Evento numero="1"/>{/*posso ter evento modificado por um valor de propriedade*/}
       <Form/>
---
+++ src/components/Condicional.js
@@ -0,0 +1,37 @@
+import { useState } from "react"
+
+function Condicional(){
+
+    const [email, setEmail] = useState()
+    const [userEmail, setUseEmail] = useState() //esse state é alterado a partir do momento que da submit no formulario
+
+    function enviarEmail(e){
+        e.preventDefault()
+        setUseEmail(email)
+    }
+    
+    function limparEmail(){
+        setUseEmail('')
+    }
+
+    return(
+        <div>
+            <h2>Cadastre seu e-mail:</h2>
+            <form>
+              <input 
+               type="email" 
+               placeholder="Digite seu e-mail..." 
+               onChange={(e) => setEmail(e.target.value)}
+               />
+              <button type="submit" onClick={enviarEmail}> Enviar e-mail</button>
+              {userEmail && ( //renderização condicional uma forma de validação (se tiver valor no input do email retorne a msg a baixo)
+                <div>
+                    <p>O e-mail do usuario é: {userEmail}</p>
+                    <button onClick={limparEmail}>Limpar e-mail</button>
+                </div>
+              )}
+            </form>
+        </div>
+    )
+}
+export default Condicional
