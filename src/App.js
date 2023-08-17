c5dcd45b2545e09e3c1c150fec3a7b7627507bb2
vanderleiacn <vanderleiacn@gmail.com>
2023-08-17 19:14:31 -0300

    renderizando por condiçao

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import './App.css';
 import Condicional from './components/Condicional';
 import Evento from './components/Evento';
 import Form from './components/Form';
+import OutraLista from './components/OutraLista';
 //import HelloWorld from './components/HelloWorld';
 //import SayMyName from './components/SayMyName';
 function App() {
+
+    const meusItens = ['React','Vue','Angular'] //lista criada no proprio componente , mas pode vir de um servidor externo ou do servidor do BD
+
+
   return (
 
     <div className="App"> {/*a tag div é usada para encapsular tudo*/}
+      <h1>Renderização de Listas</h1>
+      <OutraLista itens={meusItens}/>
+      <OutraLista itens={[]}/> {/*retorna lista vazia*/}
       <h1>Renderização Condicional</h1>
       <Condicional/>
       <h1>Testando Eventos</h1> {/* declarado aqui, porem esta vindo do arquivo index.css*/}
